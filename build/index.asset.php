97163bd87c3fc00ce97bbb71ea23c0d6eeb38f82
Steve Jonk <stevejonk1993@gmail.com>
2024-06-12 19:45:20 +0200

    initial commit

---
+++ build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => 'cf0545ee822ad2689e1d');
---
+++ build/view.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => '6e10f6312bd6adc74e96');
